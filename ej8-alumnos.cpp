d8d1699f71a4fa5bb4a995655e79d0e0e739566a
tomasguerrero04 <tomas04guerrero@gmail.com>
2024-08-19 07:54:03 -0300

    Creacion borrador ej 7 y 8

---
+++ ej8-alumnos.cpp
@@ -0,0 +1,19 @@
+/*
+Ejercicio N°8:
+
+Ejercicio: Procesamiento de Tareas Diarias
+Imagina que tienes una lista de tareas diarias. Cada elemento de esa lista tiene el nombre de una tarea diaria. Un estudio determinó que el esfuerzo necesario para una tarea puede estimarse con la cantidad de caracteres con la que se nombra. Por lo cual, deseas programar un código en C++ que te determine cuál es la tarea del listado que te implica mayor esfuerzo para ejecutarla primero.
+Para ello, implementarás un código usando un enfoque iterativo y otro código con enfoque recursivo.
+Iterativo: Implementa una función que utilice un bucle para contar el número total de caracteres en una lista de tareas.
+Recursivo: Implementa una función recursiva que realice la misma tarea.
+En ambos casos, mida la cantidad de llamadas recursivas y de iteraciones e imprimirlas en pantalla para comparar los dos métodos.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
